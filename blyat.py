1b0d87c20c122328c9dc3eaacd21e82cd5a5fa9d
anton <ant0n@pop-os.localdomain>
2021-05-03 15:45:16 +0200

    cyka blyat

---
+++ blyat.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) < 2:
+    print("\n[!] Usage: blyat <filename>")
+    sys.exit(0)
+
+filename = sys.argv[1]
+
+if __name__ == "__main__":
+
+    print("\n\n================================\n»-(¯`·.·´¯)-> " +  filename + " <-(¯`·.·´¯)-«\n================================\n\n")
+
+    fichero = open(filename, "r")
+
+    linea = fichero.readlines()
+    line = 0
+    for i in linea:
+        line += 1
+
+        output = chr(27)+"[0;36m"+str(line)+ chr(27) + "[0m" + " " + str(i)
+        print(output.strip("\n"))
+
+
+
